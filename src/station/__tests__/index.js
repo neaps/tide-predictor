4a6b17a3099455ce47d1ccb3e6f158e19a6ba87b
Kevin Miller <keveemiller@gmail.com>
2019-10-01 18:33:20 -0300

    Simple test

--- src/station/__tests__/index.js
+++ src/station/__tests__/index.js
@@ -1,4 +1,12 @@
 import mockStation from '../__mocks__/station'
 import station from '../index'
 
-test('station setup and test valid harmonics', () => {})
+test('station setup and test valid harmonics', () => {
+  let stationCreated = true
+  try {
+    const testStation = new station(mockStation)
+  } catch {
+    stationCreated = false
+  }
+  expect(stationCreated).toBeTruthy()
+})
