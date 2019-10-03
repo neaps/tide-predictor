8bc8a3121b41d249ab6db3e5510ef18e5eaefddb
Kevin Miller <keveemiller@gmail.com>
2019-10-03 08:02:43 -0300

    Stubbed out constituent

--- src/harmonics/__tests__/constituent.js
+++ src/harmonics/__tests__/constituent.js
@@ -10,3 +10,23 @@ test('Doodson number converts to cooeficient', () => {
   const coefficient = testCooefficient.doodsonNumberToCooeficient('A BZY ZZY')
   expect(coefficient).toEqual(expect.arrayContaining([1, 2, 0, -1, 0, 0, -1]))
 })
+
+test('cooeficient converts to Doodson number', () => {
+  const testCooefficient = new constituent(null, [])
+  const doodsonNumber = testCooefficient.cooeficientToDoodsonNumber([
+    1,
+    2,
+    0,
+    -1,
+    0,
+    0,
+    -1
+  ])
+  expect(doodsonNumber).toEqual('ABZYZZY')
+})
+
+test('cooeficient hashes', () => {
+  const testCooefficient = new constituent(null, [1, 2, 0, -1, 0, 0, -1])
+  const hash = testCooefficient.hash()
+  expect(hash).toEqual('120m100m1')
+})
--- src/harmonics/constituent.js
+++ src/harmonics/constituent.js
@@ -54,6 +54,54 @@ class constituent {
     })
     return coefficeint
   }
+
+  cooeficientToDoodsonNumber(coefficients) {
+    const doodsonNumber = []
+    coefficients.forEach(coefficient => {
+      doodsonNumber.push(sortedDoodson[coefficient])
+    })
+    return doodsonNumber.join('')
+  }
+
+  V() {
+    //velociy?
+    //return np.dot(self.coefficients, self.astro_values(astro))
+  }
+
+  speed(astro) {
+    //return np.dot(self.coefficients, self.astro_speeds(a))
+  }
+
+  astronimicDoodsonNumber(astro) {
+    //return [a['T+h-s'], a['s'], a['h'], a['p'], a['N'], a['pp'], a['90']]
+  }
+
+  astronomicSpeed(astro) {
+    //return np.array([each.speed for each in self.astro_xdo(a)])
+  }
+
+  astronomicValues(astro) {
+    //return np.array([each.value for each in self.astro_xdo(a)])
+  }
+
+  isEqual(constituent) {
+    /*#Consider two out of phase constituents which travel at the same speed to
+	#be identical
+	def __eq__(self, c):
+		return np.all(self.coefficients[:-1] == c.coefficients[:-1])*/
+  }
+
+  hash() {
+    const hash = []
+    this.cooeficients.forEach(coefficient => {
+      if (coefficient < 0) {
+        hash.push(`m${coefficient * -1}`)
+      } else {
+        hash.push(coefficient)
+      }
+    })
+    return hash.join('')
+  }
 }
 
 export default constituent
