04384d11039cece2d55ff3a22a7c63a5ad0f403a
Kevin Miller <keveemiller@gmail.com>
2019-10-13 22:43:27 -0300

    Added tests against existing NOAA predictions

---
+++ src/__tests__/noaa.js
@@ -0,0 +1,81 @@
+import fs, { stat } from 'fs'
+import fetch from 'node-fetch'
+import TidePrediction from '..'
+
+//Create a directory for test cache
+if (!fs.existsSync('./.test-cache')) {
+  fs.mkdirSync('./.test-cache')
+}
+
+const stations = ['9413450', '9411340', '2695535', '8761724']
+
+const getStation = (station, callback) => {
+  const filePath = `./.test-cache/${station}.json`
+  if (fs.existsSync(filePath)) {
+    fs.readFile(filePath, (err, data) => {
+      callback(JSON.parse(data))
+    })
+    return
+  }
+  const stationData = {}
+  fetch(
+    `https://tidesandcurrents.noaa.gov/mdapi/v1.0/webapi/stations/${station}/harcon.json?units=metric`
+  )
+    .then(response => {
+      return response.json()
+    })
+    .then(harmonics => {
+      stationData.harmonics = harmonics
+      return fetch(
+        `https://tidesandcurrents.noaa.gov/api/datagetter?date=recent&station=${station}&product=predictions&datum=MTL&time_zone=gmt&units=metric&format=json`
+      )
+    })
+    .then(response => {
+      return response.json()
+    })
+    .then(levels => {
+      stationData.levels = levels
+      return fetch(
+        `https://tidesandcurrents.noaa.gov/mdapi/v1.0/webapi/stations/${station}/datums.json?units=metric`
+      )
+    })
+    .then(response => {
+      return response.json()
+    })
+    .then(info => {
+      stationData.info = info
+      fs.writeFile(filePath, JSON.stringify(stationData), error => {
+        callback(stationData)
+      })
+    })
+}
+
+describe('Results compare to NOAA', () => {
+  stations.forEach(station => {
+    test(`it compares with station ${station}`, done => {
+      getStation(station, ({ harmonics, levels, info }) => {
+        const tideStation = new TidePrediction(harmonics)
+
+        let mtl = 0
+        let mllw = 0
+        info.datums.forEach(datum => {
+          if (datum.name === 'MTL') {
+            mtl = datum.value
+          }
+          if (datum.name === 'MLLW') {
+            mllw = datum.value
+          }
+        })
+        tideStation.setOffset(mtl - mllw)
+        const results = []
+        levels.predictions.forEach(prediction => {
+          const neapsPrediction = tideStation.getWaterLevelAtTime(
+            new Date(prediction.t)
+          )
+          expect(parseFloat(prediction.v)).toBeCloseTo(neapsPrediction.level, 1)
+        })
+        done()
+      })
+    })
+  })
+})
