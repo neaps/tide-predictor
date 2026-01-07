7644ff85c030c815402cdf599217e817d6821a17
Brandon Keepers <brandon@opensoul.org>
2026-01-07 09:41:14 -0500

    Simplify testing setup

--- packages/cli/test/commands/extremes.test.ts
+++ packages/cli/test/commands/extremes.test.ts
@@ -1,26 +1,35 @@
 import { describe, test, expect } from 'vitest'
-import { Config } from '@oclif/core'
 import { runCommand } from '@oclif/test'
 import nock from 'nock'
 
 nock.disableNetConnect()
 
 describe('extremes', async () => {
-  const config = await Config.load(new URL('../..', import.meta.url).pathname)
+  function run(args: string[]) {
+    return runCommand(['extremes', ...args])
+  }
+
+  test('--help', async () => {
+    const { stdout } = await run(['--help'])
+    expect(stdout).toMatch(/extremes/)
+    expect(stdout).toMatch(/--station/)
+    expect(stdout).toMatch(/--start/)
+    expect(stdout).toMatch(/--end/)
+    expect(stdout).toMatch(/--format/)
+  })
 
   test('--station <id>', async () => {
-    const { stdout } = await runCommand(
-      ['extremes', '--station', '9414290', '--start', '2026-01-01'],
-      config
-    )
+    const { stdout } = await run([
+      '--station',
+      '9414290',
+      '--start',
+      '2026-01-01'
+    ])
     expect(stdout).toMatch(/SAN FRANCISCO/)
   })
 
   test('--near 22.24,-75.75', async () => {
-    const { stdout } = await runCommand(
-      ['extremes', '--near', '22.24,-75.75'],
-      config
-    )
+    const { stdout } = await run(['--near', '22.24,-75.75'])
     expect(stdout).toMatch(/Nurse Channel/)
   })
 
@@ -30,7 +39,7 @@ describe('extremes', async () => {
       longitude: -77.3524
     })
 
-    const { stdout, error } = await runCommand(['extremes', '--ip'], config)
+    const { stdout, error } = await run(['--ip'])
     expect(error).toBeUndefined()
     expect(stdout).toMatch(/Nassau, New Providence Island/)
   })
@@ -39,62 +48,73 @@ describe('extremes', async () => {
     test('defaults to today', async () => {
       const today = new Date().toISOString().split('T')[0]
 
-      const { stdout } = await runCommand(
-        ['extremes', '--station', '9414290'],
-        config
-      )
+      const { stdout } = await run(['--station', '9414290'])
       expect(stdout).toMatch(new RegExp(today))
     })
 
     test('accepts partial date', async () => {
-      const { stdout } = await runCommand(
-        ['extremes', '--station', '9414290', '--start', '2025-12-25'],
-        config
-      )
+      const { stdout } = await run([
+        '--station',
+        '9414290',
+        '--start',
+        '2025-12-25'
+      ])
       expect(stdout).toMatch(/2025-12-25/)
     })
 
     test('accepts full date', async () => {
-      const { stdout } = await runCommand(
-        ['extremes', '--station', '9414290', '--start', '2025-12-25T00:00:00Z'],
-        config
-      )
+      const { stdout } = await run([
+        '--station',
+        '9414290',
+        '--start',
+        '2025-12-25T00:00:00Z'
+      ])
       expect(stdout).toMatch(/2025-12-25/)
     })
 
     test('with invalid input', async () => {
-      const { error } = await runCommand(
-        ['extremes', '--station', '9414290', '--start', 'invalid-date'],
-        config
-      )
+      const { error } = await run([
+        '--station',
+        '9414290',
+        '--start',
+        'invalid-date'
+      ])
       expect(error?.message).toMatch(/Invalid time value/)
     })
   })
 
   describe('--end', () => {
     test('defaults to 72 hours from start', async () => {
-      const { stdout } = await runCommand(
-        ['extremes', '--station', '9414290', '--start', '2025-12-25'],
-        config
-      )
+      const { stdout } = await run([
+        '--station',
+        '9414290',
+        '--start',
+        '2025-12-25'
+      ])
       expect(stdout).toMatch(/2025-12-27/)
     })
 
     test('accepts partial date', async () => {
-      const { stdout } = await runCommand(
-        [
-          'extremes',
-          '--station',
-          '9414290',
-          '--start',
-          '2025-12-25',
-          '--end',
-          '2025-12-26'
-        ],
-        config
-      )
+      const { stdout } = await run([
+        '--station',
+        '9414290',
+        '--start',
+        '2025-12-25',
+        '--end',
+        '2025-12-26'
+      ])
       expect(stdout).toMatch(/2025-12-25/)
       expect(stdout).not.toMatch(/2025-12-26/)
+    })
+  })
+
+  describe('--format', () => {
+    test('json', async () => {
+      const { stdout } = await run(['--station', '9414290', '--format', 'json'])
+      const result = JSON.parse(stdout)
+      expect(result.station.name).toMatch(/SAN FRANCISCO/i)
+      expect(result.datum).toEqual('MLLW')
+      expect(result.extremes.length).toBeGreaterThan(0)
     })
   })
 })
--- packages/cli/vitest.config.ts
+++ packages/cli/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineProject } from 'vitest/config'
 
 export default defineProject({
   test: {
-    disableConsoleIntercept: true
+    disableConsoleIntercept: true,
+    setupFiles: ['./test/setup.ts']
   }
 })
